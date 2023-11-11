cfa0f010d294df3b920cf45f1e9940d1b61b648b
PlayerG9 <g9yout@gmail.com>
2023-11-11 19:12:31 +0100

    allowed @KEY variables

--- src/selenium-script/util/fill.py
+++ src/selenium-script/util/fill.py
@@ -10,15 +10,21 @@ from ..exceptions import ScriptMissingVariableError
 __all__ = ['fill']
 
 
-__RE_FILL = re.compile(r'\$(?:{(?P<extname>[^}]+?)}|(?P<name>\S+))', re.I)
+__RE_CONTEXT = re.compile(
+    # ${VAR}|$VAR|@KEY (note: ${@KEY} is also possible)
+    r'\$\{([^}]+?)}|\$(\S+)|(@\S+)',
+    re.I
+)
 
 
 def fill(string: str, context: t.Dict[str, t.Any]) -> str:
     def repl(match: re.Match) -> str:
-        name = match.group("extname") or match.group("name")
+        name = match.group(match.lastindex)
         try:
-            return str(context[name])
+            value = context[name]
         except KeyError:
             raise ScriptMissingVariableError(name)
+        else:
+            return "" if value is None else str(value)
 
-    return __RE_FILL.sub(repl=repl, string=string)
+    return __RE_CONTEXT.sub(repl=repl, string=string)
