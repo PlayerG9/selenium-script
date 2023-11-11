fda2f9fe8c1762a691e734da11a02b9d1c3d8158
PlayerG9 <g9yout@gmail.com>
2023-11-11 19:11:33 +0100

    added __all__ to fix unused import message

--- src/selenium-script/util/__init__.py
+++ src/selenium-script/util/__init__.py
@@ -5,3 +5,9 @@ r"""
 from .fill import *
 from .fmt import *
 from .parsing import *
+
+__all__ = [
+    'fill',
+    'format_action',
+    'parse', 'parse_string', 'parse_int', 'parse_number', 'parse_bool', 'PARSE_MAP',
+]
